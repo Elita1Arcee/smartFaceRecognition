05bb01221a234ba571c24dea0cedcb595e9c2902
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-07-19 14:38:32 -0400

    set up text for user info display. added background styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react'
+import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import RankUserInfo from './components/RankUserInfo/RankUserInfo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
+      <RankUserInfo />
       <ImageLinkForm />
        {/*
       <FaceRecognition />*/} 
---
+++ src/components/RankUserInfo/RankUserInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RankUserInfo () {
+    return (
+        <div>
+            <div className='white f3'>
+                <p>`You have detected the faces in 5 photos`</p>
+            </div>
+        </div>
+    )
+}
+
+export default RankUserInfo;
