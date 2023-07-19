a2df9d1000887892976656d84a646bb6a9bb943b
Louisa Blackshear-Jones <louisa@louisajones.dev>
2023-07-19 12:49:08 -0400

    setup logo component and added styles

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css'
+import brain from './icons8-brain-100.png'
+
+function Logo() {
+    return (
+        <div className='ma6 mt0' style={{ height: '150px', width: '150px'}}>
+        <Tilt className='Tilt br2 shadow-2'>
+            <div className='pa3'>
+                <img style={{paddingTop: '5px'}} src={brain} alt='pic of brain logo'/>
+            </div>
+        </Tilt>
+        </div>
+    )
+}
+
+export default Logo;
